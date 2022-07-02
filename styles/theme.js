24354f2abd18762cc70b41043496d44b808ea3b7
Dan Soutar <danielsoutar@gmail.com>
2022-07-01 22:34:15 -0400

    first commit, chakra setup

---
+++ styles/theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    black: '#0C0C0C',
+    gold: '#BB892C'
+  }
+}
+
+const theme = extendTheme({ colors })
+
+export { theme }
