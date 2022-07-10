d7639e9e1c44afea78507a67a0e194ce09b8bf30
Dan Soutar <danielsoutar@gmail.com>
2022-07-10 18:22:12 -0400

    added all log section and updated our work section to use instagram API

--- components/sections/OurWork.js
+++ components/sections/OurWork.js
@@ -1,19 +1,46 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
+import Image from 'next/image'
 import { Box } from '@chakra-ui/react'
 import { Text } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 import { HmContainer } from '../HmContainer'
 import { theme } from '../../styles/theme'
+import { IG_URL } from '../../data/igUrl'
 
-const IgBox = () => (
-  <Box w='288px' h='288px' p='20px' display='grid' placeItems='center' backgroundColor={theme.colors.brand.midGrey}>
-    <ExternalLinkIcon w={12} h={12} color='white' />
-  </Box>
-)
+// const IgBox = () => (
+//   <Box w='288px' h='288px' p='20px' display='grid' placeItems='center' backgroundColor={theme.colors.brand.midGrey}>
+//     <ExternalLinkIcon w={12} h={12} color='white' />
+//   </Box>
+// )
+
+const fetchIg = async () => {
+  try {
+    return await fetch(IG_URL).then((resp) => resp.json())
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 const OurWork = () => {
+  const [igData, setIgData] = useState()
+
+  useEffect(() => {
+    const getIgData = async () => {
+      const { data } = await fetchIg()
+      setIgData(data)
+    }
+    getIgData()
+  }, [])
+
+  console.log(igData)
+
+  const igImages = igData?.slice(0, 6).map((media, idx) => {
+    return <Image src={media.media_url} alt='instagram post media' key={media.media_url} width='388px' height='388px' />
+  })
+
   return (
     <Box as='section' className='our-team-section'>
       <Box pt={16}>
@@ -25,12 +52,7 @@ const OurWork = () => {
           </Box>
         </HmContainer>
         <Box display='flex' justifyContent='center' overflowX='hidden' gap='1px'>
-          <IgBox />
-          <IgBox />
-          <IgBox />
-          <IgBox />
-          <IgBox />
-          <IgBox />
+          {igImages}
         </Box>
       </Box>
     </Box>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['www.instagram.com', 'scontent.cdninstagram.com']
+  }
 }
 
 module.exports = nextConfig
